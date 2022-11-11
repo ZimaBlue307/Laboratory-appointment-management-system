044c590f88c56a3039571a3012d9b9735315a9c4
ZimaBlue307 <85174604+ZimaBlue307@users.noreply.github.com>
2022-11-11 18:35:36 +0800

    Delete system_file directory

--- system_file/LabBooking/File.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#define ADMIN_FILE "./file/admin.txt"
-#define STUDENT_FILE "./file/student.txt"
-#define TEACHER_FILE "./file/teacher.txt"
-#define LAB_FILE "./file/lab.txt"
-#define ORDER_FILE "./file/order.txt"
